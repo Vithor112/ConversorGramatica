1c712bdfb8b21fc8b8351995c52a8331d7efb856
Vithor <vithorpileco@gmail.com>
2022-05-02 03:15:47 -0300

    Finish FSA class

--- src/main/java/com/homework/app/structs/FSA/FSA.java
+++ src/main/java/com/homework/app/structs/FSA/FSA.java
@@ -1,4 +1,30 @@
 package com.homework.app.structs.FSA;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 // Finite-state automaton
 public class FSA {
+    private final State initial;
+    private final ArrayList<State> finalStates;
+    private final ArrayList<Transition> transitions;
+
+
+    public FSA(State initial, ArrayList<State> finalStates, ArrayList<Transition> transitions) {
+        this.initial = initial;
+        this.finalStates = finalStates;
+        this.transitions = transitions;
+    }
+
+    public State getInitial() {
+        return initial;
+    }
+
+    public ArrayList<State> getFinalStates() {
+        return finalStates;
+    }
+
+    public ArrayList<Transition> getTransitions() {
+        return transitions;
+    }
 }
--- src/main/java/com/homework/app/structs/FSA/State.java
+++ src/main/java/com/homework/app/structs/FSA/State.java
@@ -1,16 +1,10 @@
 package com.homework.app.structs.FSA;
 
 public class State {
-    private final Boolean finalState;
     private final String label;
 
     public State(Boolean finalState, String label) {
-        this.finalState = finalState;
         this.label = label;
-    }
-
-    public Boolean getFinalState() {
-        return finalState;
     }
 
     public String getLabel() {
